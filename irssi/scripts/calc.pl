f461105cf2fe55cde4e6721be243b42455e8c04b
Petr Baudis <pasky@ucw.cz>
2010-06-18 19:11:36 +0200

    calc: Add new script, simple+safe perl-based calculator

---
+++ irssi/scripts/calc.pl
@@ -0,0 +1,37 @@
+use strict;
+use warnings;
+
+use Irssi;
+use Safe;
+
+use vars qw($VERSION %IRSSI);
+
+$VERSION = '0.01';
+%IRSSI = (
+    authors     => 'Petr Baudis',
+    name        => 'calc',
+    description => 'Provide a simple Perl-ish calculator',
+    license     => 'BSD',
+);
+
+sub on_public {
+	my ($server, $message, $nick, $hostmask, $channel) = @_;
+	my $cp = Irssi::settings_get_str('bot_cmd_prefix');
+
+	return unless $message =~ s/^${cp}calc\s+//;
+
+	my $compartment = new Safe();
+	# padany is crucial for Safe to work at all
+	$compartment->permit_only(qw(:base_core :base_math join padany));
+
+	my $result = $compartment->reval($message);
+	if(not defined $result) {
+		$result = "N/A";
+	}
+
+	$server->send_message($channel, "$nick: $result", 0);
+}
+
+Irssi::signal_add('message public', 'on_public');
+
+Irssi::settings_add_str('bot', 'bot_cmd_prefix', '`');
