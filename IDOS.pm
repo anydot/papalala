4ecc16a11d6f4ba28502b8548af4c3be42209c35
Petr Baudis <pasky@ucw.cz>
2010-06-23 02:27:18 +0200

    idos: New script for public transport search

---
+++ IDOS.pm
@@ -0,0 +1,139 @@
+package IDOS::RouteQuery;
+
+# This class handles IDOS route queries.
+
+use Moose;
+use Moose::Util::TypeConstraints;
+
+subtype 'Region' => as 'Str' => where { /^[a-z]+$/; };
+subtype 'Time' => as 'Str' => where { /^\d\d?:\d\d?$/; };
+
+has 'region' => (is => 'rw', isa => 'Region', required => 1);
+has 'origin' => (is => 'rw', isa => 'Str', required => 1);
+has 'dest' => (is => 'rw', isa => 'Str', required => 1);
+has 'thru' => (is => 'rw', isa => 'Str');
+# has 'when' => (is => 'rw', isa => 'Str', default => sub { time });
+
+sub execute {
+	my $self = shift;
+
+	my @routes;
+
+	my %qpar = (f => $self->origin(), t => $self->dest(), v => $self->thru());
+	my @qpar = map {
+		my $val = $qpar{$_};
+		if (defined $val) {
+			$val =~ s#/\?&=##g; ("$_=$val")
+		}
+	} keys %qpar;
+	my $uri = "http://www.idos.cz/".$self->region()."/?".join('&', @qpar, 'submit=1');
+
+	use LWP::UserAgent;
+	use HTTP::Request;
+
+	my $ua = LWP::UserAgent->new(env_proxy => 1, keep_alive => 1, timeout => 10);
+	my $request = HTTP::Request->new('GET', $uri);
+	my $response = $ua->request($request);
+	$response->is_success or die "Cannot get $uri";
+	my @data = split(/\n/, $response->decoded_content(charset=>'utf8'));
+	# print "ook? @data\n";
+	until (not $#data or $data[0] =~ /<!-- zobrazeni vysledku start -->/) {
+		shift @data;
+	}
+
+	my ($starttime, $startdate, $tottime, $totdist, $totcost, $detail, @places);
+	until (not $#data or $data[0] =~ /<!-- zobrazeni vysledku end -->/) {
+		if ($data[0] =~ /.*<th class="time.*?>(.*?)<.*/) {
+			$starttime = $1;
+
+		} elsif ($data[0] =~ /<p>Celkov. .as </) {
+			($tottime, $totdist, $totcost) = ($data[0] =~ m#<p>Celkov. .as <strong>(.*?)</strong>(?:, vzd.lenost <strong>(.*?)</strong>)?(?:, cena <strong>(.*?)</strong>)?#);
+			$data[0] =~ m#<a href="(/detail/.*?)"#;
+			$detail = "http://jizdnirady.idnes.cz" . $1;
+
+		} elsif ($data[0] =~ /<td class="(check|empty)/) {
+			my $cl = $1;
+			$data[0] =~ s#</td>##g;
+			my ($nfield) = ($data[0] =~ m#<td class="note">#);
+			my (@data) = split(/\s*<td.*?>\s*/, $data[0]);
+			shift @data; # drop anything before first <td>
+			for (@data) {
+				s/<.*?[^ >]>//g; s/&nbsp;/ /g; s/\s+/ /g; s/^\s*//; s/\s*$//; s/^>$//;
+			}
+			if ($cl eq 'check') {
+				$startdate = $data[1];
+			}
+			my $note; $note = splice(@data, 5, 1) if $nfield;
+			push @places, {
+				place => $data[2],
+				arrival => $data[3],
+				departure => $data[4],
+				note => $note,
+				line => $data[6]
+			};
+
+		} elsif ($data[0] =~ /^<\/table>/) {
+			my @conns = map {
+				my ($d, $a) = ($places[$_]->{departure}, $places[$_+1]->{arrival});
+				if ($places[$_]->{line} =~ /esun/) {
+					($d, $a) = ($places[$_]->{arrival}, $places[$_+1]->{departure});
+				}
+				my %r = (
+					'start' => $d, 'origin' => $places[$_]->{place},
+					'stop' => $a, 'dest' => $places[$_+1]->{place},
+					'by' => $places[$_]->{line}
+				);
+				defined $places[$_]->{note} and $r{note} = $places[$_]->{note};
+				IDOS::Connection->new(%r);
+			} 0..($#places - 1);
+
+			my %r = (
+				'time' => $starttime, 'date' => $startdate,
+				'traveltime' => $tottime, 'detail' => $detail,
+			);
+			defined $totcost and $r{'traveldist'} = $totdist;
+			defined $totcost and $r{'cost'} = $totcost;
+			push @routes, IDOS::Route->new(%r, 'connections' => \@conns);
+
+			@places = ();
+		}
+
+	} continue {
+		shift @data;
+	}
+
+	return @routes;
+}
+
+1;
+
+package IDOS::Route;
+
+# This class represents single route through the map, composed of several connections.
+
+use Moose;
+
+has 'time' => (is => 'rw', isa => 'Str', required => 1);
+has 'date' => (is => 'rw', isa => 'Str', required => 1);
+has 'traveltime' => (is => 'rw', isa => 'Str');
+has 'traveldist' => (is => 'rw', isa => 'Str');
+has 'cost' => (is => 'rw', isa => 'Str');
+has 'connections' => (is => 'rw', isa => 'ArrayRef[IDOS::Connection]', required => 1);
+has 'detail' => (is => 'rw', isa => 'Str');
+
+1;
+
+package IDOS::Connection;
+
+# This class represents single route through the map, composed of several connections.
+
+use Moose;
+
+has 'start' => (is => 'rw', isa => 'Time', required => 1);
+has 'stop' => (is => 'rw', isa => 'Time', required => 1);
+has 'origin' => (is => 'rw', isa => 'Str', required => 1);
+has 'dest' => (is => 'rw', isa => 'Str', required => 1);
+has 'by' => (is => 'rw', isa => 'Str', required => 1);
+has 'note' => (is => 'rw', isa => 'Str');
+
+1;
---
+++ irssi/scripts/idos.pl
@@ -0,0 +1,89 @@
+use strict;
+use warnings;
+
+use Irssi;
+use lib qw(.);
+use IDOS;
+
+use vars qw($VERSION %IRSSI);
+
+$VERSION = '0.01';
+%IRSSI = (
+    authors     => 'Petr Baudis',
+    name        => 'idos',
+    description => 'Czech public transport connection search',
+    license     => 'BSD',
+);
+
+our %lastres; # indexed by region
+
+sub on_public {
+	my ($server, $message, $nick, $hostmask, $channel) = @_;
+	my $cp = Irssi::settings_get_str('bot_cmd_prefix');
+	my $rcount = Irssi::settings_get_str('bot_idos_results');
+
+	return unless $message =~ s/^${cp}(idos|dpp|idosr)(next)?\b\s*//;
+
+	my ($cmd, $next) = ($1, $2);
+	my $region;
+
+	if ($cmd eq 'idos') {
+		$region = 'vlakyautobusymhd';
+	} elsif ($cmd eq 'dpp') {
+		$region = 'pid';
+	} elsif ($cmd eq 'idosr') {
+		$message =~ s/^(\w+)\s+//;
+		$region = lc $1;
+	}
+
+	my $r;
+	if (not $next) {
+		my @args = split /\s*--\s*/, $message;
+
+		if ($#args < 1) {
+			$server->send_message($channel, "$nick: $cp(idos | dpp | idosr REGION) from[;fromalt;fromalt2] -- to[;...] [-- thru[;...]]", 0);
+			return;
+		}
+
+		my %par = (
+			region => $region,
+			origin => $args[0],
+			dest => $args[1]
+		);
+		$par{thru} = $args[2] if $args[2];
+		my $q = IDOS::RouteQuery->new(%par);
+		$r = [ $q->execute() ];
+		if ($#$r < 0) {
+			$server->send_message($channel, "$nick: no results", 0);
+			return;
+		}
+		$lastres{$region} = $r;
+
+	} else {
+		$r = $lastres{$region};
+		if ($#$r < 0) {
+			$server->send_message($channel, "$nick: no more results", 0);
+			return;
+		}
+	}
+
+	for (1..$rcount) {
+		my $res = shift @$r;
+		next unless $res;
+		my $cl = $res->connections();
+		my $o = "";
+		$o = sprintf '[%s %s] ', $res->time(), $res->date(), $res->traveltime(), $res->traveldist(), $res->cost();
+		for (0..$#$cl) {
+			my $c = $cl->[$_];
+			$o .= sprintf '%s -- %s%s <%s> %s -- ', $c->origin(), $c->note() ? sprintf('[%s] ', $c->note()) : '', $c->start(), $c->by(), $c->stop();
+		}
+		$o .= sprintf '%s ', $cl->[$#$cl]->dest() if $#$cl >= 0;
+		$o .= sprintf '[%s] %s', join(', ', grep { defined $_ } ($res->traveltime(), $res->traveldist(), $res->cost())), $res->detail();
+		$server->send_message($channel, "$nick: $o", 0);
+	}
+}
+
+Irssi::signal_add('message public', 'on_public');
+
+Irssi::settings_add_str('bot', 'bot_cmd_prefix', '`');
+Irssi::settings_add_str('bot', 'bot_idos_results', '2');
