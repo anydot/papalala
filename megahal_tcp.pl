f508c0a2fc79db362b9378f2f68e0085745a09e1
Premysl 'Anydot' Hruby <dfenze@gmail.com>
2010-09-05 23:34:14 +0200

    megahal_tcp.pl: die&restart from within perl, don't loose any messages

--- megahal_tcp.pl
+++ megahal_tcp.pl
@@ -1,52 +1,60 @@
 #!/usr/bin/perl
-# Run in loop: while true; do perl megahal_tcp.pl ; sleep 0.2; done
+# fork son which run megahal and periodically restarts, to avoid memleaks
 
 use warnings;
 use strict;
 
-use Megahal;
 use IO::Socket::INET;
 
-our $save_interval = 3600;
-our $save_last = time;
+my $save_interval = 3600;
+my $socket;
 
-my $socket = IO::Socket::INET->new(
+sub run_megahal {
+	use Megahal;
+
+	$SIG{ALRM} = sub {Megahal::megahal_cleanup; exit 0;};
+	alarm $save_interval;
+
+	Megahal::megahal_initialize();
+
+	print "Hal started\n";
+
+	while (my $client = $socket->accept) {
+		print "Client connected\n";
+		while (my $line = $client->getline) {
+			print "Get line: $line";
+			chomp($line);
+			
+			my $response = Megahal::megahal_do_reply($line, 0);
+			$response =~ s/\s+/ /;
+			$response =~ s/\s*$/\n/;
+
+			print "Responding with: $response";
+			
+			$client->printflush($response);
+		}
+	}
+}
+
+$socket = IO::Socket::INET->new(
 	LocalAddr => ":4566",
 	Type => SOCK_STREAM,
 	ReuseAddr => 1,
 	Listen => 1,
 );
 
-# XXX: If your brain takes long time to load, you might want to move
-# this before socket creation to avoid irssi lockups.
-Megahal::megahal_initialize();
+while (1) {
+	my $pid = fork;
 
-
-print "Hal started\n";
-
-while (my $client = $socket->accept) {
-	print "Client connected\n";
-	while (my $line = $client->getline) {
-		print "Get line: $line";
-		chomp($line);
-		
-		my $response = Megahal::megahal_do_reply($line, 0);
-		$response =~ s/\s+/ /;
-		$response =~ s/\s*$/\n/;
-
-		print "Responding with: $response";
-		
-		$client->printflush($response);
-
-		# Save brain
-		if ($save_last + $save_interval < time) {
-			Megahal::megahal_cleanup();
-			# This leaks memory:
-			# Megahal::megahal_initialize();
-			# $save_last = time;
-			exit;
-		}
+	if (!defined $pid) {
+		die $!;
+	}
+	elsif ($pid) {
+		wait;
+		sleep 1;
+	}
+	else {
+		run_megahal;
 	}
 }
-
 
