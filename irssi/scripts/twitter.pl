8e9acfee3a6d422249fbba765c8ed0f70a183583
Premysl 'Anydot' Hruby <dfenze@gmail.com>
2010-09-05 23:00:11 +0200

    twitter.pl: oauth support

--- irssi/scripts/twitter.pl
+++ irssi/scripts/twitter.pl
@@ -5,7 +5,7 @@ use Irssi;
 use Irssi::Irc;
 use Net::Twitter;
 
-use vars qw($VERSION %IRSSI $twitter $username);
+use vars qw($VERSION %IRSSI $twitter $cp);
 
 $VERSION = '0.01';
 %IRSSI = (
@@ -15,26 +15,44 @@ $VERSION = '0.01';
 	description => "allows for posting small messages directly from IRC",
 );
 
-sub on_setup_changed {
-	my $password;
-
-	$username = Irssi::settings_get_str('bot_twitter_username');
-	$password = Irssi::settings_get_str('bot_twitter_password');
-
-	$twitter = Net::Twitter->new(
-		username => $username,
-		password => $password,
+sub initialize {
+	my ($c_key, $c_secret, $a_token, $a_secret) = (
+		Irssi::settings_get_str('bot_twitter_consumer_key'),
+		Irssi::settings_get_str('bot_twitter_consumer_secret'),
+		Irssi::settings_get_str('bot_twitter_access_token'),
+		Irssi::settings_get_str('bot_twitter_access_token_secret')
 	);
+
+	if ($c_key && $c_secret) {
+		$twitter = Net::Twitter->new(
+			traits => ['API::REST', 'OAuth'],
+			consumer_key => $c_key,
+			consumer_secret => $c_secret,
+		);
+
+		if ($a_token && $a_secret) {
+			$twitter->access_token($a_token);
+			$twitter->access_token_secret($a_secret);
+		}
+	}
+}
+
+sub on_setup_changed {
+	$cp = Irssi::settings_get_str('bot_cmd_prefix');
+
+	initialize;
 }
 
 sub on_public {
 	my ($server, $message, $nick, $hostmask, $channel) = @_;
-	my $cp = Irssi::settings_get_str('bot_cmd_prefix');
 	my ($twmsg);
 
 	return unless ($twmsg) = $message =~ /^${cp}tw(?:itter)?(?:\s+(.+))?$/;
 
-	if (defined $twmsg) {
+	if (!defined $twitter) {
+		$server->send_message($channel, "$nick: Neinicializovany twitter, smula", 0);
+	}
+	elsif (defined $twmsg) {
 		$twmsg =~ /^((?:[@#]\w+\s+)*)(.*)$/;
 		# $1 -- @/#prefixes $2 -- main messages 
 		my $text = "$1$nick: $2";
@@ -55,15 +73,52 @@ sub on_public {
 		}
 	}
 	else {
+		my $obj = $twitter->verify_credentials;
+
+		my $username = $obj->{screen_name};
+
 		$server->send_message($channel, "$nick: http://twitter.com/$username", 0);
 	}
 }
+
+sub cmd_bot_twitter {
+	my $data = shift;
+
+	if (!defined $twitter) {
+		Irssi::print("First set consumer key/secret (you must register tw application on your own)");
+	}
+	elsif (!$data) {
+		Irssi::print("geturl | setpin");
+	}
+	elsif ($data eq "geturl") {
+		my $url = $twitter->get_authorization_url;
+
+		Irssi::print("Authorization url: $url -- use /bot_twitter setpin <PIN> to set the pin");
+	}
+	elsif ($data =~ /^setpin (\S)$/) {
+		my ($a_token, $a_secret) = $twitter->request_access_token(verifier => $1);
+
+		Irssi::settings_set_str('bot_twitter_access_token', $a_token);
+		Irssi::settings_set_str('bot_twitter_access_token_secret', $a_secret);
+	}
+	else {
+		Irssi::print("Unknown cmd");
+	}
+}
+
+Irssi::command_bind('bot_twitter', \&cmd_bot_twitter);
 
 Irssi::signal_add('message public', \&on_public);
 Irssi::signal_add('setup changed', \&on_setup_changed);
 
 Irssi::settings_add_str('bot', 'bot_cmd_prefix', '`');
-Irssi::settings_add_str('bot', 'bot_twitter_username', '');
-Irssi::settings_add_str('bot', 'bot_twitter_password', '');
+Irssi::settings_add_str('bot', 'bot_twitter_consumer_key', '');
+Irssi::settings_add_str('bot', 'bot_twitter_consumer_secret', '');
+Irssi::settings_add_str('bot', 'bot_twitter_access_token', '');
+Irssi::settings_add_str('bot', 'bot_twitter_access_token_secret', '');
 
-on_setup_changed; # initialize
+on_setup_changed; # get cmd prefix
+
+if (!$twitter) {
+	Irssi::print("Credentials not set? Call /bot_twitter");
+}
